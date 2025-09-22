359c56e46a8cd475375ac45e3ae4c988a92fbd36
VladimirGhrejyan <vladimir.ghrejyan@gmail.com>
2025-09-22 15:55:34 -0400

    chore(nsk-1): enable versioning, set global prefix and use helmet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { env } from 'node:process';
 
 import { CustomConfigService } from '~modules/core/custom-config';
@@ -13,9 +14,15 @@ async function bootstrap() {
 
     const PORT: number = configService.get('app').port;
 
+    app.use(helmet());
+
+    app.setGlobalPrefix('api');
+
     app.enableCors({
         origin: '*',
     });
+
+    app.enableVersioning();
 
     await app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`, `Environment: ${env.NODE_ENV}`);
